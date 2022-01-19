f3f0019ee38b7bdcb613cd1b9966362e44c8583f
Yury Borodich <xkxixnxgx@yandex.ru>
2022-01-19 10:40:04 +0300

    Added tests for task 3.

--- tests/index.test.ts
+++ tests/index.test.ts
@@ -1,4 +1,4 @@
-import { getStatistics } from '../src/index';
+import { getStatistics, sortArrByNumberOfHoles } from '../src/index';
 
 test('getStatistics', () => {
     const testMarks = [{
@@ -11,4 +11,20 @@ test('getStatistics', () => {
     expect(getStatistics(testMarks)).toEqual(
         {avgMark: 4.32, hughestMark: 'Lena', lowestMark: 'Vasya'}
         );
+})
+
+test('sortArrByNumberOfHolesPositive', () => {
+    const arrNumbers = [89, 46, 0, 694, 3, -7, -4, 98571114];
+    expect(sortArrByNumberOfHoles(arrNumbers)).toEqual(
+        [3, -7, 0, -4, 46, 89, 694, 98571114]
+        );
+})
+
+test('sortArrByNumberOfHolesEmptyArr', () => {
+    const arrEmpty: number[] = [];
+    expect(sortArrByNumberOfHoles(arrEmpty)).toEqual([]);
+})
+
+test('sortArrByNumberOfHolesUndefined', () => {
+    expect(sortArrByNumberOfHoles(undefined)).toEqual(undefined);
 })
